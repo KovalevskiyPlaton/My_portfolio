f060a169004a00ab6608ae32c6f2314ed0c387ef
P-on <soulshon@yandex.ru>
2022-11-05 01:38:15 +0300

    portf_v1

---
+++ script/script.js
@@ -0,0 +1,117 @@
+//Slider => .hero-section-skills-grid <!--6 grid-->
+const sliderLine = document.querySelector('.slider-line'),
+      prevButton = document.querySelector('.button-prev'),
+      nextButton = document.querySelector('.button-next'),
+      dots = document.querySelectorAll('.dot');
+
+let position = 0, //будут передаваться новые значения left - для сладйер лайна
+    dotIndex =0   //сохранение индекс определенного слайда для последующего смены индикации
+
+
+//functions
+
+//1) nexTbutton
+
+const nextSlide =()=>{
+    if(position<(dots.length-1)*400){
+        position += 400       //размер слайда
+        dotIndex++
+          }else{
+            position=0;
+            dotIndex=0
+    }
+    sliderLine.style.left = -position+'px';
+    thsSlide(dotIndex);
+}
+
+const prevSlide =()=>{
+    if(position>0){
+        position -= 400       //размер слайда
+        dotIndex--;
+    }else{
+        position=(dots.length-1)*400;
+        dotIndex=(dots.length-1)
+    }
+    sliderLine.style.left = -position+'px';
+    thsSlide(dotIndex);
+}
+const thsSlide = (index)=>{
+    for (let dot of dots){
+        dot.classList.remove('active');
+    }
+    dots[index].classList.add('active');
+}
+//EventListeners
+
+nextButton.addEventListener('click', nextSlide)
+prevButton.addEventListener('click', prevSlide)
+
+//+dots => addEventListener
+
+dots.forEach((dot, index)=>{
+    dot.addEventListener('click', ()=>{
+        position=400*index
+        sliderLine.style.left=-position+'px';
+        dotIndex=index;
+        thsSlide(dotIndex)
+    })
+})
+
+//dots.addEventListener add activeClass
+
+setInterval(()=>{
+    nextSlide();
+}, 3000)
+
+
+
+//Timer => .hero-section-timer-grid"> <!--5 grid-->
+// let secondsTable =
+// console.log(secondsTable)
+
+//Определение переменных времени
+
+const countTime =()=>{
+    const currentDate = new Date();
+    const deadline = new Date('2023-05-01')
+    const resultDate = Math.abs(Date.parse(deadline)-Date.parse(currentDate));
+    const years = Math.floor(resultDate/(1000*60*60*24*30*12));
+    //console.log(years);
+    const months = Math.floor(resultDate/(1000*60*60*24*30))%12;
+    console.log(months);
+    const days=Math.floor(resultDate/(1000*60*60*24)%30);
+    const hours =Math.floor(resultDate/(1000*60*60)%24);
+    const minutes = Math.floor(resultDate/(1000*60)%60);
+    const seconds = Math.floor(resultDate/1000)%60;
+    document.querySelector('.t_years').innerHTML= years;
+    document.querySelector('.t_months').innerHTML= months;
+    document.querySelector('.t_days').innerHTML= days;
+    document.querySelector('.t_hours').innerHTML= addZero(hours);
+    document.querySelector('.t_minutes').innerHTML= addZero(minutes);
+    document.querySelector('.t_seconds').innerHTML = addZero(seconds);
+
+}
+
+
+setInterval(countTime, 1000)
+
+function addZero(num){
+    if (num>=0 && num<10){        //добавление 0 если значение ровно целому (пример: будет -  20 минут, 06 часов, а не  20 минут, 6 часов ... )
+        return `0${num}`;
+    }else{
+        return num;
+    }
+}
+countTime();
+
+
+//Корректировка итоговых значений с последующем выводом на рендеринг
+
+
+//
+// function updateTime(remainsSecond){
+//
+//     document.querySelector('.timInner__seconds').innerHTML(remainsSecond)
+// }
+//
+// setInterval(updateTime(remainsSecond), 1000)
